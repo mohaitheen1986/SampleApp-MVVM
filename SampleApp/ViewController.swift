659c8a2b688466ea8cc729febb1df9c5bc555f4b
Mohaitheen <smohaitheen@gmail.com>
2020-02-10 06:18:59 +0530

    Initial Commit

---
+++ SampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by Mohaitheen on 10/02/20.
+//  Copyright © 2020 Mohaitheen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
