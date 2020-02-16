34d84a6c2511bc11d7daf68f47d58544aedcc9b9
mohaitheen1986 <smohaitheen@gmail.com>
2020-02-17 00:20:30 +0530

    Update UserListsViewController.swift

--- SampleApp/Screens/UserLists/View/UserListsViewController.swift
+++ SampleApp/Screens/UserLists/View/UserListsViewController.swift
@@ -19,10 +19,7 @@ class UserListsViewController: UIViewController, UserListViewable, Loadable, UIT
         super.viewDidLoad()
         
         self.title = "User Lists"
-        
-//        tableView?.register(UINib(nibName: "UserListCell", bundle: nil), forCellReuseIdentifier: "UserListCell")
         tableView?.rowHeight = 90
-        // Do any additional setup after loading the view.
     }
     
 }
@@ -30,10 +27,8 @@ class UserListsViewController: UIViewController, UserListViewable, Loadable, UIT
 extension UserListsViewController {
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
-    }
-    
-    
-    
+    }   
+   
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return (viewModel?.numberOfUsers.value)!
     }
@@ -52,25 +47,14 @@ extension UserListsViewController {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        
-//        let vc = UIStoryboard.init(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: "UserDetailViewController") as? UserDetailViewController
-
-        
+             
         SwinInjector.defaultContainer.register(UserDetailViewModelable.self) { resolver in
             let viewModel = UserDetailViewModel(view: resolver.resolve(UserDetailViewable.self), model: (self.viewModel?.viewModelForUser(at: indexPath.row))!)
             return viewModel
         }
+        
         let view:UserDetailViewController  = SwinInjector.defaultContainer.resolve(UserDetailViewable.self) as! UserDetailViewController
-        
-        
         self.navigationController?.pushViewController(view, animated: true)
-
-
-
-       
-//        SwinInjector.defaultContainer.resolve(UserDetailViewModel(view: resolver.resolve(UserDetailViewable.self), model: nil))
-        
-//        performSegue(withIdentifier: "UserDetailViewController", sender: self)
     }
     
 }
